b8a6ca727ed7b4d9f781e014546b59d8df587a93
Purab Shakya <shakyapurab7@gmail.com>
2026-08-22 10:24:50 +0100

    Remove unnecessary whitespace and improve code formatting

--- src/main/java/com/purab/banking/project/BankSystem.java
+++ src/main/java/com/purab/banking/project/BankSystem.java
@@ -4,74 +4,41 @@ import java.util.ArrayList;
 public class BankSystem {
 
     private ArrayList<User> users;
-
     private int nextAccountNumber;
 
     public BankSystem() {
-
-        users =
-                new ArrayList<>();
-
+        users = new ArrayList<>();
         nextAccountNumber = 100001;
     }
 
-    public boolean register(
-            String username,
-            String password) {
-
+    public boolean register(String username, String password) {
         // Check username
         for (User user : users) {
-
-            if (user.getUsername()
-                    .equals(username)) {
-
+            if (user.getUsername().equals(username)) {
                 return false;
             }
         }
 
         // Encrypt password
-        String encryptedPassword =
-                AES.encrypt(password);
+        String encryptedPassword = AES.encrypt(password);
 
         // Create account
-        BankAccount account =
-                new BankAccount(
-                        nextAccountNumber,
-                        username
-                );
-
+        BankAccount account = new BankAccount(nextAccountNumber, username);
         nextAccountNumber++;
 
         // Create user
-        User user =
-                new User(
-                        username,
-                        encryptedPassword,
-                        account
-                );
-
+        User user = new User(username, encryptedPassword, account);
         users.add(user);
 
         return true;
     }
 
-    public User login(
-            String username,
-            String password) {
-
+    public User login(String username, String password) {
         for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                String decryptedPassword = AES.decrypt(user.getPassword());
 
-            if (user.getUsername()
-                    .equals(username)) {
-
-                String decryptedPassword =
-                        AES.decrypt(
-                                user.getPassword()
-                        );
-
-                if (decryptedPassword
-                        .equals(password)) {
-
+                if (decryptedPassword.equals(password)) {
                     return user;
                 }
             }
@@ -80,14 +47,9 @@ public class BankSystem {
         return null;
     }
 
-    public User findUser(
-            String username) {
-
+    public User findUser(String username) {
         for (User user : users) {
-
-            if (user.getUsername()
-                    .equals(username)) {
-
+            if (user.getUsername().equals(username)) {
                 return user;
             }
         }
