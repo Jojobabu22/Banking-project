21aa706daf5ea02cde445bc6b6c440b5902e1ddc
Purab Shakya <shakyapurab7@gmail.com>
2026-08-22 10:26:53 +0100

    Remove unnecessary spaces and consolidate code formatting

--- src/main/java/com/purab/banking/project/AES.java
+++ src/main/java/com/purab/banking/project/AES.java
@@ -5,67 +5,36 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 public class AES {
-
     static SecretKey key;
 
     static {
         try {
-            KeyGenerator generator =
-                    KeyGenerator.getInstance("AES");
-
+            KeyGenerator generator = KeyGenerator.getInstance("AES");
             generator.init(256);
-
             key = generator.generateKey();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static String encrypt(String text) {
-
         try {
-            Cipher cipher =
-                    Cipher.getInstance("AES");
-
-            cipher.init(
-                    Cipher.ENCRYPT_MODE,
-                    key
-            );
-
-            byte[] encrypted =
-                    cipher.doFinal(
-                            text.getBytes()
-                    );
-
-            return Base64.getEncoder()
-                    .encodeToString(encrypted);
-
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encrypted = cipher.doFinal(text.getBytes());
+            return Base64.getEncoder().encodeToString(encrypted);
         } catch (Exception e) {
             return null;
         }
     }
 
     public static String decrypt(String text) {
-
         try {
-            Cipher cipher =
-                    Cipher.getInstance("AES");
-
-            cipher.init(
-                    Cipher.DECRYPT_MODE,
-                    key
-            );
-
-            byte[] encrypted =
-                    Base64.getDecoder()
-                            .decode(text);
-
-            byte[] decrypted =
-                    cipher.doFinal(encrypted);
-
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encrypted = Base64.getDecoder().decode(text);
+            byte[] decrypted = cipher.doFinal(encrypted);
             return new String(decrypted);
-
         } catch (Exception e) {
             return null;
         }
